60d7306cea5725f17218490489332daee2d12f46
mattlaguardia <matthewlaguardia@gmail.com>
2016-07-03 15:24:55 -0700

    finishes challenge 2

--- challenge_2/js/app.js
+++ challenge_2/js/app.js
@@ -1,8 +1,27 @@
 console.log('challenge #2 -- app.js loaded!');
 
-// var app = angular.module("appTwo", []);
-// app.controller("WelcomeController", WelcomeController);
+var app = angular.module("appTwo", []);
+app.controller("WelcomeController", WelcomeController);
 
-// function WelcomeController(){
-//   this.full_name = "YOUR NAME"
-// }
+function WelcomeController(){
+  this.full_name = "Matthew Laguardia";
+  this.age = 26;
+  this.city = "Sacramento";
+  this.state = "California";
+  this.full_name.length
+}
+
+app.controller("ClassController", ClassController);
+
+function ClassController(){
+  this.class_name = "Gaia",
+  this.enrolled_students = 16;
+  this.start_date = "4/3/2016";
+  this.end_date = "7/12/2016";
+  this.daysRemaining = function(){
+    var todaysDate = "7/4/2016";
+    var daysLeft = Date.parse(this.end_date) - Date.now();
+    var remainder = Math.floor(daysLeft/1000/24/60/60);
+    return remainder;
+  }
+}
