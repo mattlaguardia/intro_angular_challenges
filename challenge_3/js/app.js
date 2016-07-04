01fb2f259217399af6cb205325e46b8a5a850fce
mattlaguardia <matthewlaguardia@gmail.com>
2016-07-03 17:15:24 -0700

    finishes challenge 3

--- challenge_3/js/app.js
+++ challenge_3/js/app.js
@@ -4,6 +4,29 @@ var app = angular.module("appThree", []);
 app.controller("infoController", infoController)
 
 function infoController(){
-  this.name = "Jon Doe"; // default name
-  this.height = 5.6; // default height in feet
+  this.name = "Jon Doe";
+  this.height = 5.6;
+}
+
+app.controller("numContoller", numContoller);
+
+function numContoller(){
+  this.num1 = 0;
+  this.num2 = 0;
+}
+
+app.controller("loanController", loanController);
+
+function loanController() {
+  this.amount = 1000;
+  this.apr = .08;
+  this.month = function(){
+    var yearly_interest = (this.amount * (this.apr / 100));
+    var monthly_interest = (yearly_interest / 12);
+    var accrued_interest = n * monthly_interest || 0;
+    return {
+        accrued_interest: accrued_interest,
+        total_balance: this.amount + accrued_interest;
+    }
+  }
 }
